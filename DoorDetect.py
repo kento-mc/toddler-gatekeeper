b0ac9f543b99761a7fe6a8d5eabb52524040aff7
kent <kento.mc@gmail.com>
2019-12-14 23:11:10 +0000

    Added code to parse and format accelerometer data. Still needs to be tested with actual readings of door movement to determine appropriate threshold for triggering an event. Also imported the Wia library and began setting up the flow on the Wia dashboard.

--- DoorDetect.py
+++ DoorDetect.py
@@ -1,9 +1,30 @@
 import  time
+from wia import Wia
 from sense_hat import SenseHat
 
 sense = SenseHat()
 
+wia = Wia()
+wia.access_token = "d_sk_yxhosMPKxwC9HIg2pZLxi4w3"
+
+motion = sense.get_accelerometer_raw()
+
+
 while True:
-  motion=sense.get_accelerometer_raw()
-  print(motion)
-  time.sleep(1)
+  #if accel data passes threshold
+    #publish event to wia
+  #motion=sense.get_accelerometer_raw()
+  #print(motion)
+  	x = motion['x']
+	y = motion['y']
+	z = motion['z']
+
+	x = round(x, 2)
+	y = round(y, 2)
+	z = round(z, 2)
+
+	print("x={0}, y={1}, z={2}".format(x, y, z))
+
+
+
+	time.sleep(1)
