73d6cad5c7efcfe77421edf5b9b90b3c3dd09aae
kent <kento.mc@gmail.com>
2019-12-17 12:02:34 +0000

    Creating door open condisions on the pi now writes to the database on the remote db server and sends a tweet via thingspeak. Still having the issue of only the first instance being tweeted. Since this happening with both Wia and Thingspeak I suspect it has someting to do with Twitter.

--- DoorDetect.py
+++ DoorDetect.py
@@ -1,4 +1,5 @@
 import  time
+import os
 import urllib2
 from wia import Wia
 from sense_hat import SenseHat
@@ -16,11 +17,11 @@ red = (255,0,0)
 green = (0,255,0)
 
 # create database on DB server (Macbook Pro), if it does not already exist
-mysql -uroot -h192.168.0.45 -e "CREATE DATABASE IF NOT EXISTS doorOpenDB"
-mysql -uroot -h192.168.0.45 -e "CREATE TABLE IF NOT EXISTS doorOpenDB.OpenEvents (doorOpen TIMESTAMP)"
+os.system("mysql -uroot -h192.168.0.45 --password='steviey19' -e \"CREATE DATABASE IF NOT EXISTS doorOpenDB\"")
+os.system("mysql -uroot -h192.168.0.45 --password='steviey19' -e \"CREATE TABLE IF NOT EXISTS doorOpenDB.OpenEvents (doorOpen TIMESTAMP)\"")
 
-def writeData(x) # function to send data to thingspeak
-  conn = urllib2.urlopen(baseURL + '&field1=%s&field2=%s&field3=%s' % (x))
+def writeData(x): # function to send data to thingspeak
+  conn = urllib2.urlopen(baseURL + '&field1=%s' % (x))
   print(conn.read())
   conn.close()
 
@@ -48,7 +49,7 @@ while True:
       sense.show_message("Back to bed kiddo!", text_colour = red) # display message
 
       # save door open event timestamp to DB server
-      mysql -h 192.168.0.45 -uroot -e "insert into doorOpenDB.OpenEvents(doorOpen) VALUES(CURRENT_TIMESTAMP)"
+      os.system("mysql -h 192.168.0.45 -uroot --password='steviey19' -e \"insert into doorOpenDB.OpenEvents(doorOpen) VALUES(CURRENT_TIMESTAMP)\"")
 
     x = sense.get_accelerometer_raw()['x'] # check for whether door has been closed
 
