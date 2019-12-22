124f0164ba66b900bd93ebd58d2e40c2e65e13a5
kent <kento.mc@gmail.com>
2019-12-22 14:54:25 +0000

    Final tweaks to comments DoorDetect.py file

--- DoorDetect.py
+++ DoorDetect.py
@@ -8,9 +8,11 @@ import urllib2
 from wia import Wia
 from sense_hat import SenseHat
 
+# set up Wia
 wia = Wia()
 wia.access_token = "d_sk_yxhosMPKxwC9HIg2pZLxi4w3"
 
+# set up Thingspeak
 WRITE_API_KEY='ON0J9PUVWMEQZ9RH'
 baseURL='https://api.thingspeak.com/update?api_key=%s' % WRITE_API_KEY
 
@@ -42,29 +44,29 @@ def writeData(x): # function to send data to thingspeak
   conn.close()
 
 def LEDs(): # function for LED display
-  if args == 0 or now.time() < threshold.time():
+  if args == 0 or now.time() < threshold.time(): # if there is no argument provided or if the time argument is earlier than the current time.
     sense.show_message("Back to bed kiddo!", scroll_speed = 0.035, text_colour = red) # display message
   else:
     sense.show_message("Good morning!", scroll_speed = 0.035, text_colour = green) # display message
 
 def audio(): # function for audio playback via bluetooth
-  if args == 0 or now.time() < threshold.time():
-    os.system('mpg123 -q /home/pi/dev/wit/compsys19/assignments/02/audio-files/back-to-bed.mp3')
+  if args == 0 or now.time() < threshold.time(): # if there is no argument provided or if the time argument is earlier than the current time.
+    os.system('mpg123 -q /home/pi/dev/wit/compsys19/assignments/02/audio-files/back-to-bed.mp3') # play mp3
   else:
-    os.system('mpg123 -q /home/pi/dev/wit/compsys19/assignments/02/audio-files/good-morning.mp3')
+    os.system('mpg123 -q /home/pi/dev/wit/compsys19/assignments/02/audio-files/good-morning.mp3') # play mp3
 
-while True:
+while True: # main programme loop. will run as long as script is running
   doorOpen = False # doorOpen event reset
   motion = sense.get_accelerometer_raw()
 
-  x = round(motion['x'], 4)
-  y = round(motion['y'], 4)
-  z = round(motion['z'], 4)
+  x = round(motion['x'], 4) # parse the JSON data for x value and round it
+  y = round(motion['y'], 4) # parse the JSON data for y value and round it
+  z = round(motion['z'], 4) # parse the JSON data for z value and round it
 
   # print accelerometer readings and time to console for testing purposes
   print("x={0}, y={1}, z={2}".format(x, y, z))
 
-  while x > 0.95: # door is opened
+  while x > 0.975: # door is opened
     if doorOpen == False: # if door had been closed until while loop was triggered
       doorOpen = True # set status of door to open
 
@@ -75,7 +77,7 @@ while True:
 
       writeData(x) # send data to thingspeak
 
-      wia.Event.publish(name="door open test", data = str(now.time())) # publish event to wia
+      wia.Event.publish(name="door open", data = str(now.time())) # publish event to wia
 
       if __name__ == '__main__':
         Process(target=LEDs).start() # run LED process
@@ -83,4 +85,4 @@ while True:
 
     x = sense.get_accelerometer_raw()['x'] # check for whether door has been closed
 
-  time.sleep(.5)
+  time.sleep(.5) # set delay between reads of the accelerometer data
